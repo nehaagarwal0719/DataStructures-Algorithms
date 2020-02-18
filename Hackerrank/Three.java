356c177bb408c0082e311b6f02d36aba7a18bd98
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-02-19 01:25:16 +0530

    Create Three.java

---
+++ Hackerrank/Three.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
+     
+    Scanner sc = new Scanner(System.in);
+    int n = sc.nextInt();
+    int[] arr=new int[n];
+    int res=0;
+    for (int i=0; i<n; i++){
+        arr[i]=sc.nextInt();
+        int s=arr[i];
+        if (s<0) res++;
+        for (int j=i-1; j>=0; j--){
+            s+=arr[j];
+            if (s<0) res++;
+        }
+    }
+    System.out.println(res);
+    }
+}
