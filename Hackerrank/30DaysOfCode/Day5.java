a8c547175903f388ae80544b0b531235aa6d455f
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-02-28 10:54:27 +0530

    Update Day5.java

--- Hackerrank/30DaysOfCode/Day5.java
+++ Hackerrank/30DaysOfCode/Day5.java
@@ -1,48 +1,23 @@
 import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
 import java.util.*;
+import java.util.concurrent.*;
+import java.util.regex.*;
 
-public class Person {
-    private int age;	
-  
-	public Person(int initialAge) {
-  		// Add some more code to run some checks on initialAge
-          if(initialAge<0){
-              age=0;
-            System.out.println("Age is not valid, setting age to 0.");  
-          }
-          else
-           age=initialAge;
-	}
+public class Solution {
 
-	public void amIOld() {
-  		// Write code determining if this person's age is old and print the correct statement:
-            if(age<13)
-                 System.out.print("You are young.");
-           else if(age>=13 && age<18)
-                System.out.print("You are a teenager.");
-            else if(age>=18)
-             System.out.print("You are old.");   
-        System.out.println(/*Insert correct print statement here*/);
-	}
 
-	public void yearPasses() {
-  		// Increment this person's age.
-          age=age+1;
-	}
 
-	public static void main(String[] args) {
-		Scanner sc = new Scanner(System.in);
-		int T = sc.nextInt();
-		for (int i = 0; i < T; i++) {
-			int age = sc.nextInt();
-			Person p = new Person(age);
-			p.amIOld();
-			for (int j = 0; j < 3; j++) {
-				p.yearPasses();
-			}
-			p.amIOld();
-			System.out.println();
+    private static final Scanner scanner = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int n = scanner.nextInt();
+        scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+        for(int i=1;i<=10;i++){
+            System.out.println(n+" x "+i+" = "+n*i);
         }
-		sc.close();
+        scanner.close();
     }
 }
