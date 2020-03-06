c129c87a0c2bb0b16df16c9f0d8d0257529ed1a1
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-03-06 20:02:43 +0530

    Create Problem1.js

---
+++ Hackerrank/javascript/Problem1.js
@@ -0,0 +1,23 @@
+function getArea(length, width) {
+    let area;
+    // Write your code here
+    area=length*width;
+    return area;
+}
+
+
+function getPerimeter(length, width) {
+    let perimeter;
+    // Write your code here
+    perimeter=2*(length+width);
+    return perimeter;
+}
+
+
+function main() {
+    const length = +(readLine());
+    const width = +(readLine());
+    
+    console.log(getArea(length, width));
+    console.log(getPerimeter(length, width));
+}
