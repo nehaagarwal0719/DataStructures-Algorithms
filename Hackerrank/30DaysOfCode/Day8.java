dd6807c607da9c1552f79cdfeb52fdea5ce66fad
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-03-06 12:41:51 +0530

    Create Day8.java

---
+++ Hackerrank/30DaysOfCode/Day8.java
@@ -0,0 +1,25 @@
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        Map<String,Integer> PhoneBook=new HashMap<String,Integer>();
+        for(int i = 0; i < n; i++){
+            String name = in.next();
+            int phone = in.nextInt();
+            PhoneBook.put(name,phone);
+        }
+        
+        while(in.hasNext()){
+            String s = in.next();
+            // Write code here
+            if(PhoneBook.get(s)==null)
+                System.out.println("Not found");
+            else    
+                System.out.println(s+"="+PhoneBook.get(s));
+        }
+        in.close();
+    }
+}
