3d529b5fe533dd548fcdf0276ecb942ee70513d6
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-03-12 14:07:44 +0530

    Create Day_12.java

---
+++ Hackerrank/30DaysOfCode/Day_12.java
@@ -0,0 +1,63 @@
+import java.util.*;
+
+class Person {
+	protected String firstName;
+	protected String lastName;
+	protected int idNumber;
+	
+	// Constructor
+	Person(String firstName, String lastName, int identification){
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.idNumber = identification;
+	}
+	
+	// Print person data
+	public void printPerson(){
+		 System.out.println(
+				"Name: " + lastName + ", " + firstName 
+			+ 	"\nID: " + idNumber); 
+	}
+	 
+}
+
+class Student extends Person{
+	private int[] testScores;
+    Student(String firstName, String lastName ,int id, int scores[])
+    {
+        super(firstName,lastName,id);
+        testScores=scores;
+    }
+    char calculate(){
+        int s=0;
+        for(int i=0;i<testScores.length;i++){
+            s=s+testScores[i];
+        }
+        float avg=s/testScores.length;
+         return avg >= 90? 'O'
+        : avg >= 80? 'E'
+        : avg >= 70? 'A'
+        : avg >= 55? 'P'
+        : avg >= 40? 'D'
+        : 'T';     
+    }
+}
+
+class Solution {
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		String firstName = scan.next();
+		String lastName = scan.next();
+		int id = scan.nextInt();
+		int numScores = scan.nextInt();
+		int[] testScores = new int[numScores];
+		for(int i = 0; i < numScores; i++){
+			testScores[i] = scan.nextInt();
+		}
+		scan.close();
+		
+		Student s = new Student(firstName, lastName, id, testScores);
+		s.printPerson();
+		System.out.println("Grade: " + s.calculate());
+	}
+}
