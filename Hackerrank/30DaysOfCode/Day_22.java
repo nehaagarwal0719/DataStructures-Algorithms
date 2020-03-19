f9e10379065779e671f947496bde9a3594522ebc
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-03-19 10:10:36 +0530

    Create Day_22.java

---
+++ Hackerrank/30DaysOfCode/Day_22.java
@@ -0,0 +1,59 @@
+import java.util.*;
+import java.io.*;
+class Node{
+    Node left,right;
+    int data;
+    Node(int data){
+        this.data=data;
+        left=right=null;
+    }
+}
+class Solution{
+
+	public static int getHeight(Node root){
+      //Write your code here
+      if(root==null)
+        return -1;
+      else{
+          
+          int left= getHeight(root.left);
+          int right=getHeight(root.right);
+
+          if (left>right)
+              return left + 1;
+          else
+             return right+1;    
+          
+
+      }
+    }
+
+	public static Node insert(Node root,int data){
+        if(root==null){
+            return new Node(data);
+        }
+        else{
+            Node cur;
+            if(data<=root.data){
+                cur=insert(root.left,data);
+                root.left=cur;
+            }
+            else{
+                cur=insert(root.right,data);
+                root.right=cur;
+            }
+            return root;
+        }
+    }
+    public static void main(String args[]){
+            Scanner sc=new Scanner(System.in);
+            int T=sc.nextInt();
+            Node root=null;
+            while(T-->0){
+                int data=sc.nextInt();
+                root=insert(root,data);
+            }
+            int height=getHeight(root);
+            System.out.println(height);
+        }	
+}
