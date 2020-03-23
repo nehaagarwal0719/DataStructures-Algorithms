6be17bb93448adb7680824daa6b1bc4facc10223
Neha Agarwal <nehaagarwal0464@gmail.com>
2020-03-23 13:23:04 +0530

    Create Convert_Lowercase.java

---
+++ GeeksForGeeks/String/Convert_Lowercase.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+
+		Scanner sc = new Scanner(System.in);
+		int T= sc.nextInt();
+		int c=0;
+		for(int i=1;i<=T;i++){
+		    String s =sc.next();
+		    System.out.println(s.toLowerCase()); 
+		}    
+	}
+}
