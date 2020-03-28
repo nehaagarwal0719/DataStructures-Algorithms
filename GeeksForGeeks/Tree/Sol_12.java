db35ef554ad93c2f1d665a7c73abae68156a2592
neha <nehaagarwal0464@gmail.com>
2020-03-28 17:45:03 +0530

    trees

---
+++ GeeksForGeeks/Tree/Sol_12.java
@@ -0,0 +1,7 @@
+	int minDepth(Node root)
+	{
+	   if(root == null) return 0;
+        int left = minDepth(root.left);
+        int right = minDepth(root.right);
+        return (left == 0 || right == 0) ? left + right + 1: Math.min(left,right) + 1;
+	}
