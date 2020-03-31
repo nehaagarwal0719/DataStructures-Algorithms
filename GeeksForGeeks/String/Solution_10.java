3294f81a346be18d0260c69ae06b0a0a469b6628
neha <nehaagarwal0464@gmail.com>
2020-03-31 16:19:05 +0530

    strings

---
+++ GeeksForGeeks/String/Solution_10.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+	public static void main (String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		int T=sc.nextInt();
+		
+		for(int i=1;i<=T;i++){
+		    String s=sc.next();
+		    int c=0;
+		for(int j=0;j<s.length();j++)    {
+		    if(s.charAt(j)=='2' && s.charAt(j+1)=='1')
+		    c++;
+		}
+		System.out.println(c);
+		}
+	}
+}
