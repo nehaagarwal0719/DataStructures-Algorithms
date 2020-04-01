24c7346f0fffdc668b8314d9b45f7f4eb1f0086d
neha <nehaagarwal0464@gmail.com>
2020-04-02 00:48:25 +0530

    better sol

--- LeetCode/30DaysChallenge/Day1.java
+++ LeetCode/30DaysChallenge/Day1.java
@@ -8,7 +8,67 @@ class Solution {
                 break;
             }      
         }
-        
+
         return nums[i];
     }
+}
+
+
+// BETTER WAY  
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class GFG {
+    public static void main (String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        int T=sc.nextInt();
+        
+        for(int i=1;i<=T;i++){
+            int n=sc.nextInt();
+            
+         int a[] = new int[n];
+         
+         for(int j=0;j<n;j++){
+             a[j]=sc.nextInt();
+         } 
+         
+         search(a,0,n-1);
+    
+         
+         }
+    }
+    
+    public static void search(int[] arr, int low, int high) 
+    { 
+        if(low > high) 
+            return; 
+        if(low == high) 
+        { 
+            System.out.println(arr[low]); 
+            return; 
+        } 
+          
+    
+        int mid = (low + high)/2; 
+
+        if(mid % 2 == 0) 
+        { 
+            if(arr[mid] == arr[mid+1]) 
+                search(arr, mid+2, high); 
+            else
+                search(arr, low, mid); 
+        } 
+        
+        else if(mid % 2 == 1) 
+        { 
+            if(arr[mid] == arr[mid-1]) 
+                search(arr, mid+1, high); 
+            else
+                search(arr, low, mid-1); 
+        } 
+    } 
 }
