c4c247cd27e83bc5afa2c7f12b1a51af54fcc8ca
Neha Agarwal <nehaagarwal0464@gmail.com>
2023-02-20 12:51:21 +0530

    Update Solution_28.java

--- GeeksForGeeks/LinkedList/Solution_28.java
+++ GeeksForGeeks/LinkedList/Solution_28.java
@@ -14,3 +14,20 @@ class Solution {
      return 0;   
     }
 }
+
+
+public static boolean detectLoop(Node head){
+        // Add code here
+       HashSet<Node> hs = new HashSet<Node>();
+       while(head!=null){
+           if(hs.contains(head))
+           return true;
+           
+           hs.add(head);
+           head=head.next;
+       }
+       return false;
+    }
+
+
+Time and space - O(n)
